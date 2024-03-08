dda6d1b1af40b3cadcb3dde09e2672babff825a6
wjfe <1374144742@qq.com>
2024-03-08 10:26:52 +0800

    md文档编辑

--- src/components/WmdEdit.vue
+++ src/components/WmdEdit.vue
@@ -2,6 +2,10 @@
 import "@/styles/index.scss";
 import "@/plugins/mdPlugins.js";
 import { useArticleStore } from "@/store/article.ts";
+import { ref } from "vue";
+import storage from "@/utils/storage";
+import axios from "axios";
+
 const articleStore = useArticleStore();
 const toolbar = {
   leftToolbar:
@@ -10,7 +14,48 @@ const toolbar = {
   disabledMenus: [],
 };
 
+const editor = ref();
+
 const emit = defineEmits(["update:value"]);
+
+const content = ref("");
+
+const handleUploadImage = (_event: any, _insertImage: any, files: any[]) => {
+  // 拿到 files 之后上传到文件服务器，然后向编辑框中插入对应的内容
+  let file = files[0];
+  let param = new FormData(); // 创建form对象
+  param.append("file", file, file.name); // 通过append向form对象添加数据
+  let config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      authorization: storage.get("userInfo").token,
+    },
+  };
+  axios.post(`/w1/w1/common/upload`, param, config).then(
+    (response: {
+      data: {
+        status: any;
+        data: {
+          [x: string]: any;
+          previewUrl: any;
+          filename: any;
+        };
+      };
+    }) => {
+      if (response.data.status) {
+        console.log(response);
+        editor.value.insert((_selected: any) => {
+          return {
+            text: `<a href="${response.data.data.fileUrl}" target="_blank">
+<img src="${response.data.data.fileUrl}" alt="${response.data.data.fileName}" width="750">
+</a>`,
+            selected: content,
+          };
+        });
+      }
+    }
+  );
+};
 
 // 双向数据绑定
 const handleChange = (val: any) => {
@@ -23,13 +68,14 @@ const handleChange = (val: any) => {
   <div>
     <v-md-editor
       v-model="articleStore.editArticleData.content"
-      ref="preview"
+      ref="editor"
       height="400px"
       placeholder="请输入内容"
       :left-toolbar="toolbar.leftToolbar"
       :right-toolbar="toolbar.rightToolbar"
       :disabled-menus="toolbar.disabledMenus"
       @change="handleChange"
+      @upload-image="handleUploadImage"
     ></v-md-editor>
   </div>
 </template>
